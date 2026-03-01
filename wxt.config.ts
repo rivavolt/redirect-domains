1c26dd0f2d3d061a2a78ecab24ab078109fff0c3
Andrei Volt <andrei@avolt.net>
2026-03-01 15:14:05 +0100

    add stable extension key for nix CRX signing

--- wxt.config.ts
+++ wxt.config.ts
@@ -7,5 +7,6 @@ export default defineConfig({
     version: '0.1.0',
     description: 'Redirects domains based on declarative config',
     permissions: ['storage'],
+    key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsv45GJOznwJaj7MDHdPMwj5UykeWETu6hWKWyAfNNowDrjjrlrq2ySvIBQyE488ZBHuJebtvQaTczadP2f2Vu75UKj1vAUL3ZLGa3nMN65S9tCl9BjZulGbeJjUJ+GdW2FnLzGzIu0Zf1y5RZTzJFZy13darS3guORXrj7flVsCyMqMfzQJnrFMcqjAS4UyUOUzpAhp3SPauaIgQ5TIyp3dy9h03EgrcaPd6gpt5dpPXG9pUA13alLhGt28Gy3uhcyOdPLFl4c9MBE82GeSGrj1tH929FppKt8X1QPe51SyLUkkdBuY2cTD+AajlzVlRLJYNa2Idxd/54INl5yjcNwIDAQAB',
   },
 });
